c0dbd9a803ad06ed7b0c0547ae24b26bd304a45f
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-12-13 20:57:19 +0800

    implement simple error

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import { Env } from './config/env.config';
 import cors from 'cors';
 import { HTTPSTATUS } from './config/http.config';
+import { error } from 'console';
+import { errorHandler } from './middlewares/errorHandler.middleware';
 
 const app = express();
 const BASE_PATH = Env.BASE_PATH;
@@ -18,8 +20,11 @@ app.use(
 );
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    throw new Error('Test Error Handling');
     res.status(HTTPSTATUS.OK).json({ message: 'API is running' });
 });
+
+app.use(errorHandler);
 
 app.listen(Env.PORT, () => {
     console.log(`Server is running on port ${Env.PORT}`);
