928b1a67ce762faa2f188e50c295e6046da3482f
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-12-13 20:21:23 +0800

    first setup

---
+++ dist/config/env.config.d.ts
@@ -0,0 +1,18 @@
+export declare const Env: {
+    NODE_ENV: string;
+    PORT: string;
+    BASE_PATH: string;
+    MONGO_URI: string;
+    JWT_SECRET: string;
+    JWT_EXPIRES_IN: string;
+    JWT_REFRESH_SECRET: string;
+    JWT_REFRESH_EXPIRES_IN: string;
+    GEMINI_API_KEY: string;
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+    RESEND_API_KEY: string;
+    RESEND_MAILER_SENDER: string;
+    FRONTEND_ORIGIN: string;
+};
+//# sourceMappingURL=env.config.d.ts.map
