3b75f765236bf795b235a1c208622558ae2ccd33
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2026-02-04 11:49:03 +0800

    analyticts module

--- src/config/resend.config.ts
+++ src/config/resend.config.ts
@@ -1,4 +1,4 @@
-import { Resend } from "resend";
-import { Env } from "./env.config";
+import { Resend } from 'resend';
+import { Env } from './env.config';
 
 export const resend = new Resend(Env.RESEND_API_KEY);
--- src/mailers/report.mailer.ts
+++ src/mailers/report.mailer.ts
@@ -1,40 +1,40 @@
-import { formatCurrency } from "../utils/format-currency";
-import { getReportEmailTemplate } from "./templates/report.template";
-import { sendEmail } from "./mailer";
-import { ReportType } from "../@types/report.type";
+import { formatCurrency } from '../utils/format-currency';
+import { getReportEmailTemplate } from './templates/report.template';
+import { sendEmail } from './mailer';
+import { ReportType } from '../@types/report.type';
 
 type ReportEmailParams = {
-  email: string;
-  username: string;
-  report: ReportType;
-  frequency: string;
+    email: string;
+    username: string;
+    report: ReportType;
+    frequency: string;
 };
 
 export const sendReportEmail = async (params: ReportEmailParams) => {
-  const { email, username, report, frequency } = params;
-  const html = getReportEmailTemplate(
-    {
-      username,
-      ...report,
-    },
-    frequency
-  );
+    const { email, username, report, frequency } = params;
+    const html = getReportEmailTemplate(
+        {
+            username,
+            ...report,
+        },
+        frequency
+    );
 
-  const text = `Your ${frequency} Financial Report (${report.period})
+    const text = `Your ${frequency} Financial Report (${report.period})
     Income: ${formatCurrency(report.totalIncome)}
     Expenses: ${formatCurrency(report.totalExpenses)}
     Balance: ${formatCurrency(report.availableBalance)}
     Savings Rate: ${report.savingsRate.toFixed(2)}%
 
-    ${report.insights.join("\n")}
+    ${report.insights.join('\n')}
 `;
 
-  console.log(text, "text mail");
+    console.log(text, 'text mail');
 
-  return sendEmail({
-    to: email,
-    subject: `${frequency} Financial Report - ${report.period}`,
-    text,
-    html,
-  });
+    return sendEmail({
+        to: email,
+        subject: `${frequency} Financial Report - ${report.period}`,
+        text,
+        html,
+    });
 };
